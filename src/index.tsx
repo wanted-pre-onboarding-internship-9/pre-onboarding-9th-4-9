649ef1a381373cc6a32cf100425d02f9e3a6e984
yeonkyoung <keep.ykk@gmail.com>
2023-03-20 18:18:30 +0900

    [#2] Feat : index 파일 수정

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,8 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 
 import router from './shared/Router';
-import GlobalStyle from './styles/GlobalStyle';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <>
-    <GlobalStyle />
-    <RouterProvider router={router} />
-  </>
-);
+root.render(<RouterProvider router={router} />);
