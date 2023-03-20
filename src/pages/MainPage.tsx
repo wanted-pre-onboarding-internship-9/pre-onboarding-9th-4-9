6d4cb31ec8f306387762130fc0b1f96790a2c0e4
goeunrain <boniteterain@gmail.com>
2023-03-20 11:23:06 +0900

    [#3] Feat : 페이지네이션 구현
    - react-js-pagination 라이브러리 사용

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,4 +1,7 @@
+import { useState } from 'react';
+import Pagination from 'react-js-pagination';
 import { useQuery } from 'react-query';
+import styled from 'styled-components';
 
 import { getDataAPI } from '../apis/instance';
 import { IOrderList } from '../types/type';
@@ -6,24 +9,90 @@ import { IOrderList } from '../types/type';
 const MainPage = () => {
   const { isLoading, data } = useQuery('getData', () => getDataAPI());
 
+  const [page, setPage] = useState(1);
+  const [items, setItems] = useState(50);
+
+  const handlePageChange = (page: number) => {
+    setPage(page);
+  };
+
   if (isLoading) {
     return <h1>Loading...</h1>;
   }
+
+  // console.log(items * (page - 1), items * (page - 1) + items);
+
   return (
     <>
       <h1>switchwon</h1>
-      {data?.data.map((item: IOrderList) => (
-        <ol key={item.id}>
-          <li>{item.id}</li>
-          <li>{item.transaction_time.toString()}</li>
-          <li>{item.status}</li>
-          <li>{item.customer_id}</li>
-          <li>{item.customer_name}</li>
-          <li>{item.currency}</li>
-        </ol>
-      ))}
+      {data?.data
+        .slice(items * (page - 1), items * (page - 1) + items)
+        ?.map((item: IOrderList) => (
+          <ol key={item.id}>
+            <li>{item.id}</li>
+            <li>{item.transaction_time.toString()}</li>
+            <li>{item.status}</li>
+            <li>{item.customer_id}</li>
+            <li>{item.customer_name}</li>
+            <li>{item.currency}</li>
+          </ol>
+        ))}
+      <PaginationBox>
+        <Pagination
+          activePage={page} //현재 페이지
+          itemsCountPerPage={50} //한 페이지 당 보여줄 리스트 아이템 개수
+          totalItemsCount={500} //총 아이템 개수
+          pageRangeDisplayed={5} //paginator 내에서 보여줄 페이지의 범위
+          prevPageText='<'
+          nextPageText='>'
+          onChange={handlePageChange}
+        />
+      </PaginationBox>
     </>
   );
 };
 
 export default MainPage;
+
+const PaginationBox = styled.div`
+  .pagination {
+    display: flex;
+    justify-content: center;
+    margin-top: 15px;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+  ul.pagination li {
+    display: inline-block;
+    width: 30px;
+    height: 30px;
+    border: 1px solid #e2e2e2;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1rem;
+  }
+  ul.pagination li:first-child {
+    border-radius: 5px 0 0 5px;
+  }
+  ul.pagination li:last-child {
+    border-radius: 0 5px 5px 0;
+  }
+  ul.pagination li a {
+    text-decoration: none;
+    color: #337ab7;
+    font-size: 1rem;
+  }
+  ul.pagination li.active a {
+    color: white;
+  }
+  ul.pagination li.active {
+    background-color: #337ab7;
+  }
+  ul.pagination li a:hover,
+  ul.pagination li a.active {
+    color: blue;
+  }
+`;
