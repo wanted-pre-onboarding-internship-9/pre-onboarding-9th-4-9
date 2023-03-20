22ad829e906333b08ae41a49e128ae4b955bc5cc
goeunrain <boniteterain@gmail.com>
2023-03-20 22:40:24 +0900

    [#4] Feat : 기본 정렬 / ID 기준 오름차순

--- src/hooks/useGetList.tsx
+++ src/hooks/useGetList.tsx
@@ -8,9 +8,14 @@ const useGetList = () => {
     staleTime: Infinity,
     refetchOnWindowFocus: false,
     select: data => {
-      const orderList = data?.data.filter((item: IOrderList) =>
-        item.transaction_time.includes('2023-03-08')
-      );
+      const orderList = data?.data
+        .sort(function (a: IOrderList, b: IOrderList) {
+          return a.id - b.id;
+        })
+        .filter((item: IOrderList) =>
+          item.transaction_time.includes('2023-03-08')
+        );
+
       return orderList;
     },
   });
