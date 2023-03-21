890cfb8f4625c500a434e9215edaee1e70d374d2
Nyeong <90minyeong90@gmail.com>
2023-03-21 12:10:59 +0900

    [#5] Fix : 불필요한 리렌더링을 방지하기 위해 useEffect를 사용하도록 변경

--- src/hooks/useFilter.ts
+++ src/hooks/useFilter.ts
@@ -1,72 +1,47 @@
+import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 import { OrderType } from '../types';
 
 const useFilter = (orders: OrderType[] | undefined) => {
+  const [filteredOrders, setFilteredOrders] = useState<OrderType[]>([]);
   const [searchParams] = useSearchParams();
-
   const requestedDate = searchParams.get('date') || '2023-03-08';
-  const requestedSortById = searchParams.get('sort-id');
-  const requestedSortByTime = searchParams.get('sort-time');
-  const requestedPage = Number(searchParams.get('page')) || 1;
-  const requestedFilterByStatus =
+  const requestedStatus =
     searchParams.get('status') === '완료'
       ? true
       : searchParams.get('status') === '미완료'
       ? false
       : null;
-  const requestedSearch = searchParams.get('search');
+  const requestedName = searchParams.get('search');
 
-  let filteredOrders = orders;
+  useEffect(() => {
+    let tempFilteredOrders = orders || [];
 
-  filteredOrders = orders?.filter(
-    order => requestedDate && order.transaction_time.startsWith(requestedDate)
-  );
+    if (requestedDate) {
+      tempFilteredOrders = tempFilteredOrders.filter(order =>
+        order.transaction_time.startsWith(requestedDate)
+      );
+    }
 
-  if (requestedFilterByStatus !== null) {
-    filteredOrders = orders?.filter(
-      order => order.status === requestedFilterByStatus
-    );
-  }
+    if (requestedStatus !== null) {
+      tempFilteredOrders = tempFilteredOrders.filter(
+        order => order.status === requestedStatus
+      );
+    }
 
-  if (requestedSortById) {
-    const sortById = (a: OrderType, b: OrderType) => {
-      if (requestedSortById === '내림차순') {
-        return b.id - a.id;
-      } else {
-        return a.id - b.id;
-      }
-    };
+    if (requestedName) {
+      tempFilteredOrders = tempFilteredOrders.filter(order =>
+        order.customer_name
+          .toUpperCase()
+          .includes(requestedName.toLocaleUpperCase())
+      );
+    }
 
-    filteredOrders = filteredOrders?.sort(sortById);
-  }
+    setFilteredOrders(tempFilteredOrders);
+  }, [orders, requestedDate, requestedStatus, requestedName]);
 
-  if (requestedSortByTime) {
-    const sortByTime = (a: OrderType, b: OrderType) => {
-      if (requestedSortByTime === '내림차순') {
-        return b.transaction_time.localeCompare(a.transaction_time);
-      } else {
-        return a.transaction_time.localeCompare(b.transaction_time);
-      }
-    };
-
-    filteredOrders = filteredOrders?.sort(sortByTime);
-  }
-
-  if (requestedSearch) {
-    filteredOrders = filteredOrders?.filter(order =>
-      order.customer_name
-        .toUpperCase()
-        .includes(requestedSearch.toLocaleUpperCase())
-    );
-  }
-
-  const totalPage = filteredOrders ? Math.ceil(filteredOrders.length / 50) : 0;
-  const startIndex = (requestedPage - 1) * 50;
-  const endIndex = startIndex + 50;
-  const pageItems = filteredOrders?.slice(startIndex, endIndex);
-
-  return { totalPage, pageItems };
+  return filteredOrders;
 };
 
 export default useFilter;
