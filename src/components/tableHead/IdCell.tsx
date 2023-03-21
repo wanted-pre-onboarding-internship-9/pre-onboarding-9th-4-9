e506194e3b2fc8d3a88404a4870173f016036550
Nyeong <90minyeong90@gmail.com>
2023-03-21 11:57:45 +0900

    [#4] Fix : 정렬 선택이 바뀌면 다른 정렬의 placeholder가 초기화 되도록 수정

--- src/components/tableHead/IdCell.tsx
+++ src/components/tableHead/IdCell.tsx
@@ -1,24 +1,38 @@
 import { Select, Th } from '@chakra-ui/react';
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 const IdCell = () => {
   const [searchParams, setSearchParams] = useSearchParams();
-
-  const [sortByIdPlaceholder, setSortByIdPlaceholder] = useState(
+  const [placeholder, setPlaceholder] = useState<string>(
     searchParams.get('sort-id') || '정렬 선택'
   );
+  const selectRef = useRef<HTMLSelectElement>(null);
+
+  useEffect(() => {
+    setPlaceholder(searchParams.get('sort-id') || '정렬 선택');
+  }, [searchParams]);
 
   const handleSortById: React.ChangeEventHandler<HTMLSelectElement> = event => {
-    searchParams.delete('sort-time');
-    searchParams.set('sort-id', event.target.value);
-    setSearchParams(searchParams);
+    setSearchParams(searchParams => {
+      searchParams.delete('sort-time');
+      searchParams.delete('page');
+      searchParams.set('sort-id', event.target.value);
+      return new URLSearchParams(searchParams);
+    });
+
+    if (selectRef.current) {
+      selectRef.current.selectedIndex = 0;
+    }
   };
 
   return (
     <Th scope='col'>
       주문 번호
-      <Select placeholder={sortByIdPlaceholder} onChange={handleSortById}>
+      <Select
+        placeholder={placeholder}
+        onChange={handleSortById}
+        ref={selectRef}>
         <option value='오름차순'>오름차순</option>
         <option value='내림차순'>내림차순</option>
       </Select>
--- src/components/tableHead/TimeCell.tsx
+++ src/components/tableHead/TimeCell.tsx
@@ -1,26 +1,38 @@
 import { Select, Th } from '@chakra-ui/react';
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 const TimeCell = () => {
   const [searchParams, setSearchParams] = useSearchParams();
-
-  const [sortByTimePlaceholder, setSortByTimePlaceholder] = useState(
+  const [placeholder, setPlaceholder] = useState<string>(
     searchParams.get('sort-time') || '정렬 선택'
   );
+  const selectRef = useRef<HTMLSelectElement>(null);
+
+  useEffect(() => {
+    setPlaceholder(searchParams.get('sort-time') || '정렬 선택');
+  }, [searchParams]);
 
   const handleSortByTime: React.ChangeEventHandler<
     HTMLSelectElement
   > = event => {
     searchParams.delete('sort-id');
+    searchParams.delete('page');
     searchParams.set('sort-time', event.target.value);
     setSearchParams(searchParams);
+
+    if (selectRef.current) {
+      selectRef.current.selectedIndex = 0;
+    }
   };
 
   return (
     <Th scope='col'>
       거래 시간
-      <Select placeholder={sortByTimePlaceholder} onChange={handleSortByTime}>
+      <Select
+        placeholder={placeholder}
+        onChange={handleSortByTime}
+        ref={selectRef}>
         <option value='오름차순'>오름차순</option>
         <option value='내림차순'>내림차순</option>
       </Select>
